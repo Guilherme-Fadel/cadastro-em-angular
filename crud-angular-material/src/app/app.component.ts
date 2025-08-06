9d76f93aecda75dd0360253f834616cf659895f7
guilherme-fadel <guilhermefadel13@gmail.com>
2025-08-06 17:31:25 -0300

    Faltante

--- crud-angular-material/src/app/app.component.ts
+++ crud-angular-material/src/app/app.component.ts
@@ -4,6 +4,8 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 
+
+
 @Component({
   selector: 'app-root',
   imports: [
